060a4e731373dec3c27985c6f47d01e28fe1f921
Craig Miller <craigmiller160@gmail.com>
2020-07-08 16:05:51 -0400

    Added filter configurer

---
+++ src/main/kotlin/io/craigmiller160/oauth2/security/JwtValidationFilterConfigurer.kt
@@ -0,0 +1,21 @@
+package io.craigmiller160.oauth2.security
+
+import io.craigmiller160.oauth2.config.OAuthConfig
+import io.craigmiller160.oauth2.service.TokenRefreshService
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.web.DefaultSecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.stereotype.Component
+
+@Component
+class JwtValidationFilterConfigurer (
+        private val oAuthConfig: OAuthConfig,
+        private val tokenRefreshService: TokenRefreshService
+) : SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity>() {
+
+    override fun configure(http: HttpSecurity?) {
+        http?.addFilterBefore(JwtValidationFilter(oAuthConfig, tokenRefreshService), UsernamePasswordAuthenticationFilter::class.java)
+    }
+
+}
