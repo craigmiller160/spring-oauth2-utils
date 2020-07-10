3238c08c2b4dea8e28ee8ecf8675997a4f4656ef
Craig Miller <craigmiller160@gmail.com>
2020-07-10 16:25:30 -0400

    Finished test

--- src/test/kotlin/io/craigmiller160/oauth2/service/OAuthServiceTest.kt
+++ src/test/kotlin/io/craigmiller160/oauth2/service/OAuthServiceTest.kt
@@ -1,17 +1,24 @@
 package io.craigmiller160.oauth2.service
 
+import com.nhaarman.mockito_kotlin.mock
 import io.craigmiller160.oauth2.config.OAuthConfig
 import io.craigmiller160.oauth2.repository.AppRefreshTokenRepository
+import io.craigmiller160.oauth2.security.AuthenticatedUser
 import io.craigmiller160.oauth2.testutils.JwtUtils
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.InjectMocks
 import org.mockito.Mock
 import org.mockito.Mockito
+import org.mockito.Mockito.`when`
 import org.mockito.junit.jupiter.MockitoExtension
 import org.springframework.http.ResponseCookie
 import org.springframework.security.core.Authentication
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.context.SecurityContext
 import org.springframework.security.core.context.SecurityContextHolder
 import java.time.Duration
 
@@ -29,14 +36,19 @@ class OAuthServiceTest {
     @InjectMocks
     private lateinit var oAuthService: OAuthService
 
+    @AfterEach
+    fun clean() {
+        SecurityContextHolder.clearContext()
+    }
+
     @Test
     fun test_logout() {
-        Mockito.`when`(oAuthConfig.cookieName)
+        `when`(oAuthConfig.cookieName)
                 .thenReturn(cookieName)
 
         val authentication = Mockito.mock(Authentication::class.java)
         val authUser = JwtUtils.createAuthUser()
-        Mockito.`when`(authentication.principal)
+        `when`(authentication.principal)
                 .thenReturn(authUser)
         SecurityContextHolder.getContext().authentication = authentication
 
@@ -50,7 +62,27 @@ class OAuthServiceTest {
 
     @Test
     fun test_getAuthenticatedUser() {
-        TODO("Finish this")
+        val authUser = AuthenticatedUser(
+                userName = "User",
+                grantedAuthorities = listOf(SimpleGrantedAuthority("Something")),
+                firstName = "First",
+                lastName = "Last",
+                tokenId = "ID"
+        )
+        val context = mock<SecurityContext>()
+        val authentication = mock<Authentication>()
+        `when`(context.authentication)
+                .thenReturn(authentication)
+        SecurityContextHolder.setContext(context)
+        `when`(authentication.principal)
+                .thenReturn(authUser)
+
+        val result = oAuthService.getAuthenticatedUser()
+        assertEquals("User", result.username)
+        assertEquals("First", result.firstName)
+        assertEquals("Last", result.lastName)
+        assertEquals(1, result.roles.size)
+        assertEquals("Something", result.roles[0])
     }
 
     private fun validateCookie(cookie: ResponseCookie, token: String, exp: Long) {
