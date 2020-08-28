c6d9da8ea699089fb525400a6d3a5ad0b02a4ff6
Craig Miller <craigmiller160@gmail.com>
2020-08-28 10:02:55 -0400

    Changed auth code login from get request to post request

--- src/main/kotlin/io/craigmiller160/oauth2/controller/OAuthController.kt
+++ src/main/kotlin/io/craigmiller160/oauth2/controller/OAuthController.kt
@@ -4,6 +4,7 @@ import io.craigmiller160.oauth2.dto.AuthUserDto
 import io.craigmiller160.oauth2.service.AuthCodeService
 import io.craigmiller160.oauth2.service.OAuthService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -17,7 +18,7 @@ class OAuthController (
         private val oAuthService: OAuthService
 ) {
 
-    @GetMapping("/authcode/login")
+    @PostMapping("/authcode/login")
     fun login(req: HttpServletRequest, res: HttpServletResponse) {
         val authCodeLoginUrl = authCodeService.prepareAuthCodeLogin(req)
         res.status = 302
