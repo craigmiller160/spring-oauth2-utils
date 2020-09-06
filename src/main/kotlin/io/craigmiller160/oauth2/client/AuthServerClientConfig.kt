43d668753f2acc022adfa760af33cc36cb593d2e
Craig Miller <craigmiller160@gmail.com>
2020-09-06 14:11:13 -0400

    Feature/infra 53 (#3)
    * Got it kind of sort of working
    
    * Finished updating all the logic to handle the rest template properly
    
    * Removed TODO

--- src/main/kotlin/io/craigmiller160/oauth2/client/AuthServerClientConfig.kt
+++ src/main/kotlin/io/craigmiller160/oauth2/client/AuthServerClientConfig.kt
@@ -2,13 +2,9 @@ package io.craigmiller160.oauth2.client
 
 import io.craigmiller160.oauth2.config.OAuthConfig
 import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.boot.web.client.RestTemplateCustomizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.client.BufferingClientHttpRequestFactory
-import org.springframework.http.client.SimpleClientHttpRequestFactory
-import org.springframework.http.converter.FormHttpMessageConverter
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
-import org.springframework.web.client.RestTemplate
 
 @Configuration
 class AuthServerClientConfig (
@@ -16,16 +12,15 @@ class AuthServerClientConfig (
 ) {
 
     @Bean
-    fun authRestTemplateBuilder(): RestTemplateBuilder {
-        return RestTemplateBuilder()
-//                .requestFactory { BufferingClientHttpRequestFactory(SimpleClientHttpRequestFactory()) }
-                .interceptors(listOf(RequestResponseLoggingInterceptor()))
-                .messageConverters(FormHttpMessageConverter(), MappingJackson2HttpMessageConverter())
+    fun formRestTemplateCustomizer(): RestTemplateCustomizer {
+        return RestTemplateCustomizer { template ->
+            template.interceptors.add(RequestResponseLoggingInterceptor())
+        }
     }
 
     @Bean
-    fun authServerClient(restTemplate: RestTemplate): AuthServerClient {
-        return AuthServerClientImpl(restTemplate, oAuthConfig)
+    fun authServerClient(restTemplateBuilder: RestTemplateBuilder): AuthServerClient {
+        return AuthServerClientImpl(restTemplateBuilder.build(), oAuthConfig)
     }
 
 }
