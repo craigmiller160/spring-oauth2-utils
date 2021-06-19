8e9020e008c8ade935fe4add83cc348d2b609443
Craig Miller <craigmiller160@gmail.com>
2021-06-19 12:01:51 -0400

    Refactored to properly rely on abstract parent class

--- src/main/kotlin/io/craigmiller160/spring/oauth2/config/OAuth2ConfigImpl.kt
+++ src/main/kotlin/io/craigmiller160/spring/oauth2/config/OAuth2ConfigImpl.kt
@@ -18,22 +18,17 @@
 
 package io.craigmiller160.spring.oauth2.config
 
-import com.nimbusds.jose.jwk.JWKSet
 import io.craigmiller160.oauth2.config.OAuth2Config
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Configuration
 import org.springframework.validation.annotation.Validated
-import java.net.URL
 import javax.annotation.PostConstruct
-import javax.validation.constraints.Min
 import javax.validation.constraints.NotBlank
 
 @Configuration
 @Validated
 @ConfigurationProperties(prefix = "oauth2")
-class OAuth2ConfigImpl : OAuth2Config {
+class OAuth2ConfigImpl : OAuth2Config() {
 
     @field:NotBlank(message = "Missing Property: oauth2.auth-server-host")
     override var authServerHost: String = ""
@@ -52,52 +47,9 @@ class OAuth2ConfigImpl : OAuth2Config {
     override var insecurePaths: String = ""
     override var authCodeWaitMins: Long = 10
 
-    val jwkPath = "/jwk"
-    val tokenPath = "/oauth/token"
-    val authCodeLoginPath = "/ui/login"
-
-    private val log: Logger = LoggerFactory.getLogger(javaClass)
-
-    lateinit var jwkSet: JWKSet
-
-    // TODO migrate a lot of these methods into the library
-
-    fun getOrDefaultCookiePath(): String {
-        if (cookiePath.isNotBlank()) {
-            return cookiePath
-        }
-        return "/"
-    }
-
-    fun getBaseWait(): Long {
-        return 1000
-    }
-
-    fun loadJWKSet(): JWKSet {
-        return JWKSet.load(URL("$authServerHost$jwkPath"))
-    }
-
-    fun getInsecurePathList(): List<String> {
-        return insecurePaths.split(",")
-                .map { it.trim() }
-                .filter { it.isNotBlank() }
-    }
-
     @PostConstruct
-    fun tryToLoadJWKSet() {
-        log.info("Loading JWKSet")
-        for (i in 0 until 5) {
-            try {
-                jwkSet = loadJWKSet()
-                log.debug("Successfully loaded JWKSet")
-                return
-            } catch (ex: Exception) {
-                log.error("Error loading JWKSet", ex)
-                Thread.sleep(getBaseWait() * (i + 1))
-            }
-        }
-
-        throw RuntimeException("Failed to load JWKSet")
+    fun setup() {
+        tryToLoadJWKSet()
     }
 
 }
