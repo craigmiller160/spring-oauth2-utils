6293f8d8b0bae9471cf263a6d2d939d97b4a17d4
Craig Miller <craigmiller160@gmail.com>
2021-07-16 13:54:01 -0400

    Feature/oauth2 client (#6)
    * Added TODOs
    
    * Wrote buildExecuteRequest
    
    * Integrated new AuthServerClientImpl into config
    
    * Made some progress on test refactor
    
    * More work on error handling
    
    * Fixed compile errors
    
    * Deleted unnnecessary test
    
    * Integrating client from library
    
    * Added TODO
    
    * Code cleanup

--- src/main/kotlin/io/craigmiller160/spring/oauth2/client/AuthServerClientConfig.kt
+++ src/main/kotlin/io/craigmiller160/spring/oauth2/client/AuthServerClientConfig.kt
@@ -19,29 +19,20 @@
 package io.craigmiller160.spring.oauth2.client
 
 import io.craigmiller160.oauth2.client.AuthServerClient
+import io.craigmiller160.oauth2.client.AuthServerClientImpl
 import io.craigmiller160.oauth2.config.OAuth2Config
-import org.springframework.boot.web.client.RestTemplateBuilder
-import org.springframework.boot.web.client.RestTemplateCustomizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class AuthServerClientConfig (
-        @Suppress("SpringJavaInjectionPointsAutowiringInspection")
         private val oAuthConfig: OAuth2Config
 ) {
 
     @Bean
-    fun formRestTemplateCustomizer(): RestTemplateCustomizer {
-        return RestTemplateCustomizer { template ->
-            template.interceptors.add(RequestResponseLoggingInterceptor())
-        }
-    }
-
-    @Bean
     @Suppress("SpringJavaInjectionPointsAutowiringInspection")
-    fun authServerClient(restTemplateBuilder: RestTemplateBuilder): AuthServerClient {
-        return AuthServerClientImpl(restTemplateBuilder.build(), oAuthConfig)
+    fun authServerClient(): AuthServerClient {
+        return AuthServerClientImpl(oAuthConfig)
     }
 
 }
