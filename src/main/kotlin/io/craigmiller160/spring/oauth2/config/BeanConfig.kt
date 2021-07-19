4e4c0f3dcb0dd741432399cddfcfee90306dbff1
Craig Miller <craigmiller160@gmail.com>
2021-07-19 06:58:03 -0400

    Updated BeanConfig

--- src/main/kotlin/io/craigmiller160/spring/oauth2/config/BeanConfig.kt
+++ src/main/kotlin/io/craigmiller160/spring/oauth2/config/BeanConfig.kt
@@ -1,12 +1,16 @@
 package io.craigmiller160.spring.oauth2.config
 
+import io.craigmiller160.oauth2.client.AuthServerClient
+import io.craigmiller160.oauth2.client.AuthServerClientImpl
 import io.craigmiller160.oauth2.config.OAuth2Config
 import io.craigmiller160.oauth2.domain.repository.AppRefreshTokenRepository
 import io.craigmiller160.oauth2.domain.repository.impl.AppRefreshTokenRepositoryImpl
 import io.craigmiller160.oauth2.security.AuthenticatedUser
 import io.craigmiller160.oauth2.security.CookieCreator
 import io.craigmiller160.oauth2.service.OAuth2Service
+import io.craigmiller160.oauth2.service.RefreshTokenService
 import io.craigmiller160.oauth2.service.impl.OAuth2ServiceImpl
+import io.craigmiller160.oauth2.service.impl.RefreshTokenServiceImpl
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.core.context.SecurityContextHolder
@@ -16,6 +20,12 @@ import javax.sql.DataSource
 class BeanConfig {
     @Bean
     fun cookieCreator(): CookieCreator = CookieCreator()
+
+    @Bean
+    @Suppress("SpringJavaInjectionPointsAutowiringInspection")
+    fun authServerClient(oAuth2Config: OAuth2Config): AuthServerClient {
+        return AuthServerClientImpl(oAuth2Config)
+    }
 
     @Bean
     fun appRefreshTokenRepository(oAuth2Config: OAuth2Config, dataSource: DataSource): AppRefreshTokenRepository {
@@ -33,5 +43,10 @@ class BeanConfig {
             cookieCreator
     ) {
         SecurityContextHolder.getContext().authentication.principal as AuthenticatedUser
+    }
+
+    @Bean
+    fun refreshTokenService(appRefreshTokenRepo: AppRefreshTokenRepository, authServerClient: AuthServerClient): RefreshTokenService {
+        return RefreshTokenServiceImpl(appRefreshTokenRepo, authServerClient)
     }
 }
