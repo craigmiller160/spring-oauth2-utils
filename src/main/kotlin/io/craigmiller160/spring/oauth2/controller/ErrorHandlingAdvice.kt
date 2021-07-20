fb5b786800c2fcca23289e302f9199a14d07a925
Craig Miller <craigmiller160@gmail.com>
2021-07-20 15:43:57 -0400

    Refactored exception handler and compile errors

--- src/main/kotlin/io/craigmiller160/spring/oauth2/controller/ErrorHandlingAdvice.kt
+++ src/main/kotlin/io/craigmiller160/spring/oauth2/controller/ErrorHandlingAdvice.kt
@@ -1,6 +1,9 @@
 package io.craigmiller160.spring.oauth2.controller
 
+import io.craigmiller160.oauth2.exception.BadAuthCodeRequestException
+import io.craigmiller160.oauth2.exception.BadAuthCodeStateException
 import io.craigmiller160.oauth2.exception.BadAuthenticationException
+import io.craigmiller160.spring.oauth2.exception.SpringBadAuthCodeStateException
 import io.craigmiller160.spring.oauth2.exception.SpringBadAuthenticationException
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
@@ -9,9 +12,20 @@ import org.springframework.web.bind.annotation.ExceptionHandler
 class ErrorHandlingAdvice {
 
     // TODO need to validate that this actually accomplishes its goal
+
     @ExceptionHandler(BadAuthenticationException::class)
     fun handleBadAuthenticationException(ex: BadAuthenticationException) {
         throw SpringBadAuthenticationException(ex)
+    }
+
+    @ExceptionHandler(BadAuthCodeRequestException::class)
+    fun handleBadAuthCodeRequestException(ex: BadAuthCodeRequestException) {
+        throw SpringBadAuthenticationException(ex)
+    }
+
+    @ExceptionHandler(BadAuthCodeStateException::class)
+    fun handleBadAuthCodeStateException(ex: BadAuthCodeStateException) {
+        throw SpringBadAuthCodeStateException(ex)
     }
 
 }
