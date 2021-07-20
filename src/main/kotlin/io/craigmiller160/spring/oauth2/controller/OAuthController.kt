84e6d44edbc01d44ae7a42c781b75dc2d952e63b
Craig Miller <craigmiller160@gmail.com>
2021-07-20 16:23:50 -0400

    Refactored controller

--- src/main/kotlin/io/craigmiller160/spring/oauth2/controller/OAuthController.kt
+++ src/main/kotlin/io/craigmiller160/spring/oauth2/controller/OAuthController.kt
@@ -20,9 +20,12 @@ package io.craigmiller160.spring.oauth2.controller
 
 import io.craigmiller160.oauth2.dto.AuthCodeLoginDto
 import io.craigmiller160.oauth2.dto.AuthUserDto
-import io.craigmiller160.oauth2.resource.ResourceConstants
+import io.craigmiller160.oauth2.endpoint.OAuth2Endpoint
+import io.craigmiller160.oauth2.endpoint.PathConstants
 import io.craigmiller160.oauth2.service.AuthCodeService
 import io.craigmiller160.oauth2.service.OAuth2Service
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -32,35 +35,42 @@ import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
 @RestController
-@RequestMapping(ResourceConstants.ROOT_PATH)
+@RequestMapping(PathConstants.ROOT_PATH)
 class OAuthController (
         private val authCodeService: AuthCodeService,
         private val oAuthService: OAuth2Service
-) {
+) : OAuth2Endpoint<ResponseEntity<*>> {
 
-    @PostMapping(ResourceConstants.AUTHCODE_LOGIN_PATH)
-    fun login(req: HttpServletRequest): AuthCodeLoginDto {
+    @Autowired
+    private lateinit var req: HttpServletRequest
+
+    @PostMapping(PathConstants.AUTHCODE_LOGIN_PATH)
+    override fun login(): ResponseEntity<AuthCodeLoginDto> {
         val authCodeLoginUrl = authCodeService.prepareAuthCodeLogin(req)
-        return AuthCodeLoginDto(authCodeLoginUrl)
+        return ResponseEntity.ok(AuthCodeLoginDto(authCodeLoginUrl))
     }
 
-    @GetMapping(ResourceConstants.AUTHCODE_CODE_PATH)
-    fun code(@RequestParam("code") code: String, @RequestParam("state") state: String, req: HttpServletRequest, res: HttpServletResponse) {
+    @GetMapping(PathConstants.AUTHCODE_CODE_PATH)
+    override fun code(@RequestParam("code") code: String, @RequestParam("state") state: String): ResponseEntity<Void> {
         val (cookie, postAuthRedirect) = authCodeService.code(req, code, state)
-        res.status = 302
-        res.addHeader("Location", postAuthRedirect)
-        res.addHeader("Set-Cookie", cookie)
+        return ResponseEntity.status(302)
+                .header("Location", postAuthRedirect)
+                .header("Set-Cookie", cookie)
+                .build()
     }
 
-    @GetMapping(ResourceConstants.LOGOUT_PATH)
-    fun logout(res: HttpServletResponse) {
+    @GetMapping(PathConstants.LOGOUT_PATH)
+    override fun logout(): ResponseEntity<Void> {
         val cookie = oAuthService.logout()
-        res.addHeader("Set-Cookie", cookie)
+        return ResponseEntity.status(200)
+                .header("Set-Cookie", cookie)
+                .build()
     }
 
-    @GetMapping(ResourceConstants.AUTH_USER_PATH)
-    fun getAuthenticatedUser(): AuthUserDto {
-        return oAuthService.getAuthenticatedUser()
+    @GetMapping(PathConstants.AUTH_USER_PATH)
+    override fun getAuthenticatedUser(): ResponseEntity<AuthUserDto> {
+        val authUser = oAuthService.getAuthenticatedUser()
+        return ResponseEntity.ok(authUser)
     }
 
 }
